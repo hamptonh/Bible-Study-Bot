41883893de955a44b50a68542a34ac753f0132cf
Hunter Hampton <hamptonh.dev@gmail.com>
2021-02-01 01:43:51 -0600

    Indev v0.0.1a ~ Core functionality prototype

--- bot.py
+++ bot.py
@@ -1,21 +1,19 @@
+#DISCORD.PY IMPORTS
+import discord
+from discord.ext import commands
+
+#ESV API IMPORTS
 import re
-import random
 import requests
+import random
 
-
+#ESV API STUFF
 API_KEY = '6a783febcf5d65cff19161bb669734229dccfaa7'
 API_URL = 'https://api.esv.org/v3/passage/text/'
 
-CHAPTER_LENGTHS = [
-  14, 18, 19, 16, 14, 20, 28, 13, 28, 39, 40
-]
+def get_passage(book, chapter, verse):
 
-
-def get_passage():
-    chapter = random.randrange(1, len(CHAPTER_LENGTHS))
-    verse = random.randint(1, CHAPTER_LENGTHS[chapter])
-
-    return 'Hebrews %s:%s' % (chapter, verse)
+    return str(book) + '%s:%s' % (chapter, verse)
 
 
 def get_esv_text(passage):
@@ -46,6 +44,19 @@ def render_esv_text(data):
     return '%s – %s' % (text, data['canonical'])
 
 
-if __name__ == '__main__':
-    for i in range(3):
-        print(get_esv_text(get_passage()))
+client = commands.Bot(command_prefix='.')
+
+QUERY = []
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('ESV.org'))
+    print("Welcome Mr. Stark")
+
+@client.command()
+async def bsearch(ctx, arg, arg2, arg3):
+    text = get_esv_text(get_passage(arg, arg2, arg3))
+    await ctx.send(text)
+
+client.run('NzU0MjE0OTAwNTE0OTQ3MTAz.X1xfXA.LTBcmry9mMUFbrdpw8XBcqgvq6Q')
